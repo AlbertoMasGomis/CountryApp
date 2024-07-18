ccaaf70c7650321b1b98d948f2e4e6e1f952ef60
Alberto Mas <amasgomis@gmail.com>
2024-07-18 21:46:41 +0200

    CountryTable component 1/2

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CountriesService } from '../../service/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -6,7 +8,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styles: ``,
 })
 export class ByCapitalPageComponent {
-  searchByCapital(tem: string): void {
-    console.log(tem);
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
+
+  searchByCapital(term: string): void {
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
 }
