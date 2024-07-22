6fa0f73106a3bf8a8890fa77d1b38173e7a70ee6
Alberto Mas <alberto.mas@nunsys.com>
2024-07-22 11:47:44 +0200

    Sección 9 finalizada

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../service/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = [];
+  constructor(private countryService: CountriesService) {}
 
+  searchCountry(term: string) {
+    return this.countryService
+      .searchCountry(term)
+      .subscribe((countries) => (this.countries = countries));
+  }
 }
